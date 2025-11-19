6e5a5dcc583e9f6ac51268758485c83239a7c646
Dolly Suman <dolly.suman.023@gmail.com>
2025-11-19 20:34:25 +0530

    entry next

--- Backend/Routes/Budget.js
+++ Backend/Routes/Budget.js
@@ -7,14 +7,14 @@ const { enterAmt, totalExp}  = require("../Controller/logic");
 
 
 router.get("/home", (req,res) => {
-    // res.sendFile(path.join(process.cwd(), "Frontend", "home.html"));
-    res.render("home");
+    res.sendFile(path.join(process.cwd(), "Frontend", "home.html"));
+    
 
 })
 
 router.get("/entry", (req,res)=> {
-    // res.sendFile(path.join(process.cwd(), "Frontend", "entry.html")) 
-    res.render("entry")
+    res.sendFile(path.join(process.cwd(), "Frontend", "entry.html")) 
+    
 })
 
 
--- Backend/server.js
+++ Backend/server.js
@@ -8,11 +8,6 @@ const router =  require('./Routes/Budget')
 app.use(cors());
 app.use(express.json())
 
-app.set('view engine', 'ejs')
-app.set('views', path.join(__dirname, '..Frontend'));
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, '../Frontend')));
-
 app.use("/", router)
 console.log(router)
 
