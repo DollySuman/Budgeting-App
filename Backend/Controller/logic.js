a12d83008e1973e98a210805937ff2d1dc597939
Dolly Suman <dolly.suman.023@gmail.com>
2025-11-20 21:26:13 +0530

    get total

--- Backend/Controller/logic.js
+++ Backend/Controller/logic.js
@@ -2,12 +2,12 @@ const fs = require("fs")
 
 
 
-function loadDb(){
-    if(fs.existsSync("Db.json")){
-        try{
+function loadDb() {
+    if (fs.existsSync("Db.json")) {
+        try {
 
-          const data =   fs.readFileSync("Db.json","utf-8")
-          return JSON.parse(data);
+            const data = fs.readFileSync("Db.json", "utf-8")
+            return JSON.parse(data);
         } catch {
             return [];
         }
@@ -15,41 +15,42 @@ function loadDb(){
     return [];
 }
 
-function enterAmt(req,res){
+function enterAmt(req, res) {
     let newEntry = req.body;
 
-     newEntry.price = Number(newEntry.price);
+    newEntry.price = Number(newEntry.price);
     let db = loadDb();
     if (!newEntry.name || !newEntry.price || !newEntry.month) {
         return res.status(400).send("Missing required fields: name, price, or month");
     }
 
-    
+
     const exists = db.find(item => item.name === newEntry.name && item.price === newEntry.price && item.month === newEntry.month);
 
-    if(exists){
+    if (exists) {
         return res.send("Entry is already added in the Database")
     }
 
     db.push(newEntry)
 
-    fs.writeFileSync("Db.json", JSON.stringify(db,null,2));
+    fs.writeFileSync("Db.json", JSON.stringify(db, null, 2));
     res.send("Amount saved successfully");
+    res.send()
 
 }
 
-function totalExp(req,res){
+function totalExp(req, res) {
     let db = loadDb();
     let total = 0;
-   let monthexp = db.filter(e => e.month === month) ; // to get month by the user
-    
-   monthexp.forEach(e => {    
-        total += e.price;  
+    let monthexp = db.filter(e => e.month === month); // to get month by the user
+
+    monthexp.forEach(e => {
+        total += e.price;
     });
-      res.send(`The total expense is ${total}`);
+    res.send(`${total}`);
 
 
- 
+
 }
 
-module.exports =  {enterAmt , totalExp };
+module.exports = { enterAmt, totalExp };
